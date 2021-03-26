bb9c6834ad011d5a1bb808807c064586b00fc926
Daniel Knapp <knapparatus@gmail.com>
2021-03-26 15:15:34 -0500

    updated access authorization redirects for if user is not logged in

--- app.py
+++ app.py
@@ -326,7 +326,7 @@ def messages_add():
 def messages_show(message_id):
     """Show a message."""
 
-    msg = Message.query.get(message_id)
+    msg = Message.query.get_or_404(message_id)
     return render_template('messages/show.html', message=msg)
 
 
@@ -338,13 +338,15 @@ def messages_destroy(message_id):
         flash("Access unauthorized.", "danger")
         return redirect("/")
 
-    msg = Message.query.get(message_id)
+    msg = Message.query.get_or_404(message_id)
+    if msg.user_id != g.user.id:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
     db.session.delete(msg)
     db.session.commit()
 
     return redirect(f"/users/{g.user.id}")
-
-
 
 ##############################################################################
 # Homepage and error pages
